8308e63d61337fb307d14185cc336028167dfa0f
vivieloi <vivieloi22@gmail.com>
2025-02-14 14:14:03 +0000

    Primeiros códigos

---
+++ Aula00/Media.js
@@ -0,0 +1,13 @@
+let n1 = 10;
+let n2 = 7;
+let n3 = 10;
+let media = (n1 + n2 + n3) / 3;
+let status
+
+if(media >= 6) {
+    status = "aprovado";
+} else {
+    status = "Reprovado"
+}
+
+console.log("Sua média é ") + media + "voce está " + status;
---
+++ Aula00/Variaveis.js
@@ -0,0 +1,6 @@
+let nome = "Vivian"
+let idade = 17;
+let altura = 1.57;
+let isProfessor = true;
+
+console.log(typeof(nome));
