cb607326eb98bc50a4aeb4a6d5f84ede9f6bdcd4
vivieloi <vivieloi22@gmail.com>
2025-03-21 14:18:09 +0000

    Atividade 01 - Aprendendo JavaScript

---
+++ Atividades01-TiposDados/Questao01.js
@@ -0,0 +1,9 @@
+let n1 = 5
+let n2 = 4
+let n3 = 3
+let n4 = 2
+let n5 = 1
+let text1 = "Aprendi"
+let text2 = "JavaScrip"
+
+console.log(n1, ",", n2, ",", n3, ",",  n4, ",",  n5, ",",  "...",  text1, text2, "!!!");
