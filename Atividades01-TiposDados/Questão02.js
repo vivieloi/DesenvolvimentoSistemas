ca49937676db065ec97889359f3f07a7fa5fb5b2
vivieloi <vivieloi22@gmail.com>
2025-03-21 19:08:53 +0000

    Questão 03

---
+++ Atividades01-TiposDados/Questão02.js
@@ -0,0 +1,32 @@
+let t1 = typeof(0);
+let t2 = typeof (3.14);
+let t3 = typeof (Infinity);
+let t4 = typeof (NaN);
+let t5 = typeof (true);
+let t6 = typeof (4 > 1);
+let t7 = typeof ("texto");
+let t8 = typeof ("É trata" + 1994);
+let t9 = typeof (Symbol ("id"));
+let t10 = typeof (Date);
+let t11 = typeof (String);
+let t12 = typeof ([1,2,3,4]);
+let t13 = typeof (null);
+let t14 = typeof (undefined);
+let t15 = typeof (alert);
+
+console.log (t1);
+console.log (t2);
+console.log (t3);
+console.log (t4);
+console.log (t5);
+console.log (t6);
+console.log (t7);
+console.log (t8);
+console.log (t9);
+console.log (t10);
+console.log (t11);
+console.log (t12);
+console.log (t13);
+console.log (t14);
+console.log (t15);
+
